e85cdc7934b071152f708bea0250c43da4b46dd9
= <cspence5@live.com>
2015-12-01 22:04:36 +0000

    Heroku push

--- client/js/auth/auth.factory.js
+++ client/js/auth/auth.factory.js
@@ -3,9 +3,9 @@
 
 
 
-var THE_GIVING_TREE_API_HOST = 'https://localhost:3000';
+//var THE_GIVING_TREE_API_HOST = 'https://localhost:3000';
 //var THE_GIVING_TREE_API_HOST = 'givingtrees.herokuapp.com:3000';
-//THE_GIVING_TREE_API_HOST = 'https://10.99.21.59:3000';
+var THE_GIVING_TREE_API_HOST = 'https://10.99.21.59:3000';
 
 myApp.factory('AuthenticationFactory', function($window) {
   var auth = {
