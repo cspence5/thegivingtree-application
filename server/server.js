edcce323282161422e0c52e1211c6960859ec149
= <cspence5@live.com>
2015-12-02 02:00:20 +0000

    Heroku push

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.all('/*', function(req, res, next) {
   // CORS headers
   res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.setHeader('Access-Control-Allow-Origin', 'https://givingtrees.herokuapp.com/*');
   // Set custom headers for CORS
   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
   if (req.method == 'OPTIONS') {
